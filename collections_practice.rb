9c3e5351e2c97cafec52376aa8313e6db24767ef
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 22:22:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,7 +1,7 @@
 
 def begins_with_r(array)
   array.keep_if do |word|
-    if (word.split('').first != 'r' || word.split('').first != 'R')
+    if ((word.split('')).first != 'r' || word.split('').first != 'R')
       return false
     end 
   end 
